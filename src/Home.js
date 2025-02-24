5d326f84abe0c6860811d978e62bf84eeec99ca7
trevorcoyle23 <126537890+trevorcoyle23@users.noreply.github.com>
2025-02-23 19:46:54 -0600

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,8 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Link to="/SignIn"><button>Sign In</button></Link>
-            <Link to="/SignUp"><button>Sign Up</button></Link>
+            <Link to="/signin"><button>Sign In</button></Link>
+            <Link to="/signup"><button>Sign Up</button></Link>
         </div>
     );
 } export default Home;
